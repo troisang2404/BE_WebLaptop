54b13fc326bad4f4ad19db2d90f9e4dd878f7f14
troisang2404 <18130165@st.hcmuaf.edu.vn>
2022-07-19 14:59:34 +0700

    tao chuc nang dang ki dang nhap su dung Spring Security JWT (Json Web Token) + Hibernate

--- src/main/java/fit/nlu/weblaptop/security/WebSecurityConfig.java
+++ src/main/java/fit/nlu/weblaptop/security/WebSecurityConfig.java
@@ -1,4 +1,68 @@
 package fit.nlu.weblaptop.security;
 
-public class WebSecurityConfig {
+import fit.nlu.weblaptop.security.jwt.JwtAuthFilter;
+import fit.nlu.weblaptop.security.jwt.JwtEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtEntryPoint jwtEntryPoint;
+
+    @Bean
+    public JwtAuthFilter authenticationJwtTokenFilter() {
+        return new JwtAuthFilter();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)// Cung cap userservice cho spring security
+                .passwordEncoder(passwordEncoder()); // cung cấp password encoder
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Password encoder, để Spring Security sử dụng mã hóa mật khẩu người dùng
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors() // Ngăn chặn request từ một domain khác
+                .and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .authorizeRequests().antMatchers("/auth/**").permitAll()
+//                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/**").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
+                .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
+
+        // Thêm một lớp Filter kiểm tra jwt
+        httpSecurity.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
