bd6e4ec024defb08734b1bf6ddbc4a419fb80e0d
troisang2404 <18130165@st.hcmuaf.edu.vn>
2022-08-02 02:56:14 +0700

    api upload hinh anh cho san pham

--- src/main/java/fit/nlu/weblaptop/WebLaptopApplication.java
+++ src/main/java/fit/nlu/weblaptop/WebLaptopApplication.java
@@ -1,9 +1,14 @@
 package fit.nlu.weblaptop;
 
+import fit.nlu.weblaptop.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class WebLaptopApplication {
 
     public static void main(String[] args) {
